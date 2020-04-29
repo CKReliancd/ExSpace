9e064a087674b2bc4bbdf8fab8dcc9d37a20cfb7
Reliance <2065339786@qq.com>
2020-04-29 23:33:18 +0800

    to gitee

--- TomcatEx/src/main/java/ex01/pyrmont/HttpServer.java
+++ TomcatEx/src/main/java/ex01/pyrmont/HttpServer.java
@@ -1,7 +1,5 @@
 package ex01.pyrmont;
 
-import sun.misc.Request;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,16 +10,31 @@ import java.net.Socket;
 
 public class HttpServer {
 
+    /**
+     * WEB_ROOT是HTML和其他文件所在的目录
+     * 对于这个包，WEB_ROOT是工作目录下的“webroot”目录。
+     * 工作目录是文件系统中调用java命令的位置。
+     */
     public static final String WEB_ROOT =
-            System.getProperty("user.dir") + File.separator + "webroot";
+            System.getProperty("user.dir") + File.separator + "src/main/webroot";
 
-    // shutdown command
-    private static final String SHUTDOWN_COMMAND = "/SHUTDOWN";
+    //shutdown command
+    private static final String SHUTDOWM_COMMAND = "/SHUTDOWN";
 
     // the shutdown command received
     private boolean shutdown = false;
 
+    public static void main(String[] args) {
+        HttpServer httpServer = new HttpServer();
+        httpServer.await();
+    }
+
+    /**
+     * 在指定端口上等待HTTP请求，对其进行处理，然后发送响应信息回客户端。在接收到关闭命令之前，会一直保持等待状态。
+     * backlog:在拒绝接受传入请求之前，传入的连接请求的最大队列长度。
+     */
     public void await() {
+        //await方法会先创建一个ServerSocket实例，然后进入while循环
         ServerSocket serverSocket = null;
         int port = 8080;
         try {
@@ -30,28 +43,46 @@ public class HttpServer {
             e.printStackTrace();
             System.exit(1);
         }
-
+        //Loop waiting for a request
         while (!shutdown){
             Socket socket = null;
-            InputStream input = null;
-            OutputStream output = null;
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
             try {
+                //接收到请求之后，从accept方法返回的Socket实例中获取inputStream和outputStream对象
                 socket = serverSocket.accept();
-                input = socket.getInputStream();
-                output = socket.getOutputStream();
+                inputStream = socket.getInputStream();
+                outputStream = socket.getOutputStream();
 
+                //然后，创建ex01.pyrmont.Request对象，并调用其parse方法来解析HTTP请求的原始数据
+                Request request = new Request(inputStream);
+                request.parse();
 
+                //然后，创建一个Response对象，并分别调用其setRequest()方法和sendStaticResponse方法：
+                Response response = new Response(outputStream);
+                response.setRequest(request);
+                response.sendStaticResource();
 
-
-
-
+                //最后，await方法关闭套接字，调用Request类的方法来测试HTTP请求的URI是否关闭命令。
+                socket.close();
+                shutdown = request.getUri().equalsIgnoreCase(SHUTDOWM_COMMAND);
             } catch (Exception e) {
                 e.printStackTrace();
+                continue;
             }
 
 
 
+
         }
+
+
+
+
+
+
+
+
 
 
 
--- TomcatEx/src/main/java/ex01/pyrmont/Response.java
+++ TomcatEx/src/main/java/ex01/pyrmont/Response.java
@@ -1,4 +1,74 @@
 package ex01.pyrmont;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/*
+  HTTP Response = Status-Line
+    *(( general-header | response-header | entity-header ) CRLF)
+    CRLF
+    [ message-body ]
+    Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
+*/
 public class Response {
+
+    private static final int BUFFER_SIZE = 1024;
+    Request request;
+    OutputStream outputStream;
+
+    public Response(OutputStream outputStream) {
+        this.outputStream = outputStream;
+    }
+
+    /**
+     * 接受一个Request作为参数
+     *
+     * @param request
+     */
+    public void setRequest(Request request) {
+        this.request = request;
+    }
+
+    /**
+     * 发送一个静态资源到浏览器，如HTML文件。他首先会传入父路径和子路径
+     * 到File类的构造函数中来实例化java.io.File类，然后检查该文件是否
+     * 存在。若存在，sendStaticResource()方法会使用File对象创建一个
+     * java.io.FileInputStream对象。然后它调用FileInputStream类的read()
+     * 方法，并将字节数组写入到OutputStream输出。
+     * 静态资源的内容作为原始数据发送到浏览器
+     */
+    public void sendStaticResource() throws IOException {
+
+        byte[] bytes = new byte[BUFFER_SIZE];
+        FileInputStream fileInputStream = null;
+        try {
+            File file = new File(HttpServer.WEB_ROOT, request.getUri());
+            if (file.exists()) {
+                fileInputStream = new FileInputStream(file);
+                int ch = fileInputStream.read(bytes, 0, BUFFER_SIZE);
+                while (ch != -1) {
+                    outputStream.write(bytes, 0, ch);
+                    ch = fileInputStream.read(bytes, 0, BUFFER_SIZE);
+                }
+            } else {
+                //file not found
+                String errorMessage = "HTTP/1.1 404 File Not Found\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "\r\n" +
+                        "<h1>File Not Found</h1>";
+                outputStream.write(errorMessage.getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null)
+                fileInputStream.close();
+        }
+
+
+    }
+
+
 }
