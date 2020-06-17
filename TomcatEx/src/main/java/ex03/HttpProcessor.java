7802a8e57f2a0dfd0a63c97b623b2cba4cfd0038
Reliance <2065339786@qq.com>
2020-06-17 23:13:52 +0800

    第三章Request开头

---
+++ TomcatEx/src/main/java/ex03/HttpProcessor.java
@@ -0,0 +1,12 @@
+package ex03;
+
+public class HttpProcessor {
+    /**
+     * The HttpConnector with which this processor is associated
+     */
+    private HttpConnector connector = null;
+
+    public HttpProcessor(HttpConnector connector){
+
+    }
+}
