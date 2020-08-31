1a248b33739ddf0f2b07453146f6929e54fee2f2
Reliance <2065339786@qq.com>
2020-08-31 23:48:28 +0800

    快速排序完，栈溢出不是很理解

--- DataStruct/src/main/java/atguigu/sort/QuickSort.java
+++ DataStruct/src/main/java/atguigu/sort/QuickSort.java
@@ -11,10 +11,11 @@ public class QuickSort {
 
     public static void main(String[] args) {
 
-        int[] arr = {-9, 78, 0, 23, -567, 70, -1, 900, 4561};
+//        int[] arr = {-9, 78, 0, 23, -567, 70};
+        int[] arr = {-9, 78, 0, 23, -567, 70, -1, 900, -4561};
 
         int left = 0;
-        int right = arr.length;
+        int right = arr.length - 1;
 
         System.out.println("排序之前：" + Arrays.toString(arr));
 
@@ -32,53 +33,54 @@ public class QuickSort {
      * @param left
      * @param right
      */
-    private static void quickSort(int[] arr, int left, int right) {
+    public static void quickSort(int[] arr, int left, int right) {
+
+        int l = left;
+        int r = right;
+
         //pivot中轴值
         int pivot = arr[(left + right) / 2];
 
-        //临时变量
-        int temp = 0;
-
         //循环遍历数组，找出比pivot大或者小的值
-        while (left < right) {
-
+        while (l < r) {
             //左边的下标不停的右移，直到找到比arr[pivot]大的值
-            if (arr[left] < pivot) {
-                left++;
+            while (arr[l] < pivot) {
+                l++;
             }
-
             //右边的下表不停的左移，直到找到比arr[pivot]小的值
-            if (pivot < right) {
-                right--;
+            while (arr[r] > pivot) {
+                r--;
             }
-
             //如果左边下表大过右边，说明左小右大的顺序已经做好，需要递归再次确定
-            if (left > right) {
+            if (l >= r) {
                 break;
             }
-
             //如果左边或者右边存在比arr[pivot]大或者小的值，则开始交换
-            swap(arr, left, right);
-
+            swap(arr, l, r);
             //如果交换完后发现，左边的值等于pivot,则往左边再移一位
-            if (arr[left] == pivot) {
-                right--;
+            if (arr[l] == pivot) {
+                r--;
             }
-
             //如果交换完后发现，右的值等于pivot,则往右边再移一位
-            if (pivot == arr[right]) {
-                left++;
+            if (pivot == arr[r]) {
+                l++;
             }
+        }
+
+        //事实证明，必须让临时下标不满足上面的l<r才不会栈溢出
+        if (l == r) {
+            l++;
+            r--;
         }
 
         //向左递归
-        if (left < pivot) {
-            quickSort(arr, left, right);
+        if (left < r) {
+            quickSort(arr, left, r);
         }
 
         //向右递归
-        if (pivot > right) {
-            quickSort(arr, left, right);
+        if (right > l) {
+            quickSort(arr, l, right);
         }
     }
 
@@ -90,7 +92,8 @@ public class QuickSort {
      * @param right
      */
     private static void swap(int[] arr, int left, int right) {
-        int temp;
+        //临时变量
+        int temp = 0;
         temp = arr[left];
         arr[left] = arr[right];
         arr[right] = temp;
--- DataStruct/src/main/java/atguigu/sort/SortTest.java
+++ DataStruct/src/main/java/atguigu/sort/SortTest.java
@@ -6,21 +6,25 @@ import java.util.Arrays;
 /**
  * @author Administrator
  * @Description <p>
+ * <p>
+ * 80W级别
  * ----冒泡排序：
  * ---------i7770HQ花了12秒多<p>
  * ---------Intel(R) Core(TM) i5-8500 CPU @ 3.20GHz (6 CPUs), ~3.0GHz 花了9.19秒<p>
  * ----选择排序：1.723秒
- * ----插入排序：0.5秒
- * ----希尔排序: 0.020秒
+ * ----插入排序: 80W：0.5秒
+ * 8KW级别
+ * ----希尔排序: 80W：0.020秒  8KW: 31.68 秒
+ * ----快速排序: 80W：0.017秒  8KW：12.842 秒
  * @Date 2020/8/3 13:59
  */
 public class SortTest {
     public static void main(String[] args) {
 
-        int[] arr = new int[80000];
+        int[] arr = new int[80000000];
 
-        for (int i = 0; i < 80000; i++) {
-            arr[i] = (int) (Math.random() * 8000000);
+        for (int i = 0; i < 80000000; i++) {
+            arr[i] = (int) (Math.random() * 80000000);
         }
 
 //        System.out.println("排序前的数组：" + Arrays.toString(arr));
@@ -32,7 +36,9 @@ public class SortTest {
 
 //        InsertSort.inserSort(arr);
 
-        ShellSort.shellSort(arr);
+//        ShellSort.shellSort(arr);
+
+        QuickSort.quickSort(arr, 0, arr.length - 1);
 
         System.out.println("排序后的时间：" + LocalDateTime.now());
 //        System.out.println("排序后的数组：" + Arrays.toString(arr));
