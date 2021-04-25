5edae4991cf0b2661f51e36eb30335c0f3f378e1
Reliance <2065339786@qq.com>
2021-04-25 20:47:22 +0800

    sd

--- ArtConcurrentBook_EX/src/main/java/chapter04/ConnectionPool.java
+++ ArtConcurrentBook_EX/src/main/java/chapter04/ConnectionPool.java
@@ -3,12 +3,9 @@ package chapter04;
 import java.sql.Connection;
 import java.util.LinkedList;
 
-/**
- * 6-16
- */
 public class ConnectionPool {
 
-    private LinkedList<Connection> pool = new LinkedList<Connection>();
+    private LinkedList<Connection> pool = new LinkedList<>();
 
     public ConnectionPool(int initialSize) {
         if (initialSize > 0) {
@@ -18,17 +15,6 @@ public class ConnectionPool {
         }
     }
 
-    public void releaseConnection(Connection connection) {
-        if (connection != null) {
-            synchronized (pool) {
-                // 添加后需要进行通知，这样其他消费者能够感知到链接池中已经归还了一个链接
-                pool.addLast(connection);
-                pool.notifyAll();
-            }
-        }
-    }
-
-    // 在mills内无法获取到连接，将会返回null
     public Connection fetchConnection(long mills) throws InterruptedException {
         synchronized (pool) {
             // 完全超时
@@ -55,4 +41,16 @@ public class ConnectionPool {
             }
         }
     }
+
+    public void releaseConnection(Connection connection) {
+        if (connection != null) {
+            synchronized (pool) {
+                //连接释放后需要进行通知，这样其他消费者
+                // 能够感知到连接池中已经归还了一个连接
+                pool.addLast(connection);
+                pool.notifyAll();
+            }
+        }
+    }
+
 }
--- ArtConcurrentBook_EX/src/main/java/chapter04/ConnectionPoolTest.java
+++ ArtConcurrentBook_EX/src/main/java/chapter04/ConnectionPoolTest.java
@@ -4,8 +4,8 @@ import java.sql.Connection;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
-
 public class ConnectionPoolTest {
+
     static ConnectionPool pool = new ConnectionPool(10);
     // 保证所有ConnectionRunner能够同时开始
     static CountDownLatch start = new CountDownLatch(1);
@@ -13,7 +13,7 @@ public class ConnectionPoolTest {
     static CountDownLatch end;
 
     public static void main(String[] args) throws Exception {
-        // 线程数量，可以线程数量进行观察 ArtConcurrentBook_EX/src/main/java/chapter04/ConnectionPoolTest.java
+        // 线程数量，可以线程数量进行观察
         int threadCount = 20;
         end = new CountDownLatch(threadCount);
         int count = 20;
@@ -25,30 +25,34 @@ public class ConnectionPoolTest {
             thread.start();
         }
         start.countDown();
-        end.await();
         System.out.println("total invoke: " + (threadCount * count));
+        System.out.println("got connection:  " + got);
+        System.out.println("not got connection " + notGot.get());
     }
 
     static class ConnectionRunner implements Runnable {
         int count;
         AtomicInteger got;
-        AtomicInteger noGot;
+        AtomicInteger notGot;
 
-        public ConnectionRunner(int count, AtomicInteger got, AtomicInteger noGot) {
+        public ConnectionRunner(int count, AtomicInteger got, AtomicInteger notGot) {
             this.count = count;
             this.got = got;
-            this.noGot = noGot;
+            this.notGot = notGot;
         }
 
         @Override
         public void run() {
             try {
                 start.await();
-            } catch (InterruptedException e) {
+            } catch (Exception ex) {
+
             }
             while (count > 0) {
                 try {
-                    Connection connection = pool.fetchConnection(1000);
+                    // 从线程池中获取连接，如果1000ms内无法获取到，将会返回null
+                    // 分别统计连接获取的数量got和未获取到的数量notGot
+                    Connection connection = pool.fetchConnection(100);
                     if (connection != null) {
                         try {
                             connection.createStatement();
@@ -58,13 +62,14 @@ public class ConnectionPoolTest {
                             got.incrementAndGet();
                         }
                     } else {
-                        noGot.incrementAndGet();
+                        notGot.incrementAndGet();
                     }
-                } catch (Exception e) {
+                } catch (Exception ex) {
                 } finally {
                     count--;
                 }
             }
+            end.countDown();
         }
     }
 }
