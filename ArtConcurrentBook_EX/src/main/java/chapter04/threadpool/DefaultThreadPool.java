73c607754db147b23bf233bb23b234da32532934
Reliance_CK <2065339786@qq.com>
2021-04-26 17:58:02 +0800

    SimpleHttpServer

--- ArtConcurrentBook_EX/src/main/java/chapter04/threadpool/DefaultThreadPool.java
+++ ArtConcurrentBook_EX/src/main/java/chapter04/threadpool/DefaultThreadPool.java
@@ -6,7 +6,16 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-public class DefaultThreadPoolPool<Job extends Runnable> implements ThreadPool<Job> {
+/**
+ * 线程池的本质就是使用了一个线程安全的工作队列连接工作者线程和客户端
+ * 线程，客户端线程将任务放入工作队列后便返回，而工作者线程则不断地从工作队列上取出
+ * 工作并执行。当工作队列为空时，所有的工作者线程均等待在工作队列上，当有客户端提交了
+ * 一个任务之后会通知任意一个工作者线程，随着大量的任务被提交，更多的工作者线程会被唤醒
+ *
+ * @param <Job>
+ */
+public class DefaultThreadPool<Job extends Runnable> implements ThreadPool<Job> {
+
     // 线程池最大限制数
     private static final int MAX_WORKS_NUMBERS = 10;
     // 线程池默认的数量
@@ -23,15 +32,30 @@ public class DefaultThreadPoolPool<Job extends Runnable> implements ThreadPool<J
     // 线程编号的生成
     private AtomicLong threadNum = new AtomicLong();
 
-    public DefaultThreadPoolPool() {
+    public DefaultThreadPool() {
         initiallizeWorks(DEFAULT_WORKS_NUMBERS);
     }
 
-    public DefaultThreadPoolPool(int num) {
+    public DefaultThreadPool(int num) {
         workerNum = num > MAX_WORKS_NUMBERS ?
                 MAX_WORKS_NUMBERS :
                 num < MIN_WORKER_NUMBERS ?
                         MIN_WORKER_NUMBERS : num;
+    }
+
+    /**
+     * 初始化工作者线程
+     *
+     * @param num
+     */
+    private void initiallizeWorks(int num) {
+        for (int i = 0; i < num; i++) {
+            Worker worker = new Worker();
+            workers.add(worker);
+            Thread thread = new Thread(worker, "ThreadPool-Worker-" +
+                    threadNum.incrementAndGet());
+            thread.start();
+        }
     }
 
     @Override
@@ -39,9 +63,11 @@ public class DefaultThreadPoolPool<Job extends Runnable> implements ThreadPool<J
         if (job != null) {
             synchronized (jobs) {
                 jobs.addLast(job);
-                //添加一个Job后，对工作队列jobs调用了其notify()方法，而不是notifyAll()方法，因为能够
-                //确定有工作者线程被唤醒，这时使用notify()方法将会比notifyAll()方法获得更小的开销（避免
-                //将等待队列中的线程全部移动到阻塞队列中）
+                /**
+                 * 添加一个Job后，对工作队列jobs调用了其notify()方法，而不是notifyAll()方法，因为能够
+                 * 确定有工作者线程被唤醒，这时使用notify()方法将会比notifyAll()方法获得更小的开销（避免
+                 * 将等待队列中的线程全部移动到阻塞队列中）
+                 */
                 jobs.notify();
             }
         }
@@ -66,17 +92,23 @@ public class DefaultThreadPoolPool<Job extends Runnable> implements ThreadPool<J
         }
     }
 
+    /**
+     * 这里结束线程的方法就是worker跑完run方法
+     *
+     * @param num
+     */
     @Override
     public void removeWorks(int num) {
         synchronized (jobs) {
             if (num >= this.workerNum) {
                 throw new IllegalArgumentException("beyond workNum");
             }
-            //按照给定的数量停止Worker
+            // 按照给定的数量停止Worker
             int count = 0;
             while (count < num) {
                 Worker worker = workers.get(count);
                 if (workers.remove(worker)) {
+                    // running = false ,run方法跑完，线程结束
                     worker.shutDown();
                     count++;
                 }
@@ -91,28 +123,20 @@ public class DefaultThreadPoolPool<Job extends Runnable> implements ThreadPool<J
     }
 
     /**
-     * 初始化工作者线程
-     *
-     * @param num
+     * 从线程池的实现可以看到，当客户端调用execute(Job)方法时，会不断地向任务列表jobs中
+     * 添加Job，而每个工作者线程会不断地从jobs上取出一个Job进行执行，当jobs为空时，工作者线
+     * 程进入等待状态。
      */
-    private void initiallizeWorks(int num) {
-
-        for (int i = 0; i < num; i++) {
-            Worker worker = new Worker();
-            workers.add(worker);
-            Thread thread = new Thread(worker,
-                    "ThreadPool-Worker-" + threadNum.incrementAndGet());
-            thread.start();
-        }
-    }
-
-
     class Worker implements Runnable {
         //是否工作
         private volatile boolean running = true;
 
         @Override
         public void run() {
+            /**
+             * running = false ,run方法跑完，线程结束
+             * running = true , run方法一直取工作列表的任务运行
+             */
             while (running) {
                 Job job = null;
                 synchronized (jobs) {
@@ -121,13 +145,14 @@ public class DefaultThreadPoolPool<Job extends Runnable> implements ThreadPool<J
                             jobs.wait();
                         } catch (InterruptedException e) {
                             // 感知到外部对WorkerThread的中断操作，返回
-                            java.lang.Thread.currentThread().interrupt();
+                            Thread.currentThread().interrupt();
                             return;
                         }
                     }
                     job = jobs.removeFirst();
                 }
                 if (job != null) {
+                    // 从工作列表取一个工作运行
                     job.run();
                 }
             }
