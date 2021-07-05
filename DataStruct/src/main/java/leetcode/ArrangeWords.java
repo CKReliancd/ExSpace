966fe6db63c570e2aaafa6fd9c39612292440503
Reliance_CK <2065339786@qq.com>
2021-07-05 16:36:31 +0800

    算法

---
+++ DataStruct/src/main/java/leetcode/ArrangeWords.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Administrator
+ * @Description
+ * @Date 2021/6/28 17:02
+ */
+public class ArrangeWords {
+
+    public static void main(String[] args) {
+
+        String text = "Leetcode is cool";
+        String text1 = "To be or not to be";
+        String result = arrangeWords1(text1);
+        System.out.println(result);
+    }
+
+
+    private static String arrangeWords2(String text) {
+
+        String[] split = StringUtils.split(text, " ");
+
+        List<String> collect = Arrays.stream(split)
+                .sorted(Comparator.comparingInt(s -> s.length()))
+                .collect(Collectors.toList());
+
+        String result = StringUtils.join(collect, " ");
+
+        return result;
+    }
+
+    private static String arrangeWords1(String text) {
+        String[] split = StringUtils.split(text, " ");
+        List<String> collect = Arrays.stream(split)
+                .sorted(Comparator.comparingInt(s -> s.length()))
+                .collect(Collectors.toList());
+        String result = StringUtils.join(collect, " ");
+        return result;
+    }
+
+
+}
---
+++ DataStruct/src/main/java/leetcode/Foo.java
@@ -0,0 +1,77 @@
+package leetcode;
+
+import java.text.MessageFormat;
+
+public class Foo {
+
+//    private int firstJobDown = 0;
+//
+//    private int sencondeJobDown = 0;
+
+    private volatile int firstJobDown = 0;
+
+    private volatile int sencondeJobDown = 0;
+
+//    private AtomicInteger firstJobDown = new AtomicInteger(0);
+//
+//    private AtomicInteger sencondeJobDown = new AtomicInteger(0);
+
+    public Foo() {
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Foo foo = new Foo();
+
+        new Thread(() -> {
+            try {
+                foo.first(() -> System.out.println("first"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                foo.second(() -> System.out.println("second"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                foo.third(() -> System.out.println("third"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+    }
+
+    public void first(Runnable printFirst) {
+        System.out.println("第一个线程执行");
+        printFirst.run();
+        firstJobDown = firstJobDown + 1;
+    }
+
+    public void second(Runnable printSecond) {
+        int i = 1;
+        while (firstJobDown != 1) {
+            i++;
+        }
+        System.out.println(MessageFormat.format("第二线程竞争:{0}次后执行", i));
+        printSecond.run();
+        sencondeJobDown = sencondeJobDown + 1;
+    }
+
+    public void third(Runnable printThird) {
+        int i = 1;
+        while (sencondeJobDown != 1) {
+            i++;
+        }
+        System.out.println(MessageFormat.format("第三线程竞争:{0}次后执行", i));
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+
+
+}
